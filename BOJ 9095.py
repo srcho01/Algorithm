bdd6f5ff66f4225fd72d02ef9ac5f12a06d44a48
srcho01 <srcho01@naver.com>
2023-07-13 17:41:38 +0900

    9095 수정

--- BOJ 9095.py
+++ BOJ 9095.py
@@ -5,7 +5,7 @@ d[1] += [(1,0,0)]
 d[2] += [(2,0,0), (0,1,0)]
 d[3] += [(3,0,0), (1,1,0), (0,0,1)]
 
-for i in range(4, 12):
+for i in range(4, 11):
     for j in range(1, i//2+1):
         for a, b, c in d[j]:
             for x, y, z in d[i-j]:
