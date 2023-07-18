1525b64285291ae98d2593b8f94bda2dbff37412
srcho01 <seorim5306@gmail.com>
2023-07-18 23:15:09 +0900

    230718

---
+++ BOJ 15650.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+arr = []
+
+def dfs():
+    for i in range(1, n+1):
+        if len(arr) < m and (len(arr) == 0 or i > arr[-1]):
+            arr.append(i)
+            dfs()
+        elif len(arr) == m:
+            print(*arr)
+            arr.pop()
+            return
+    if len(arr) > 0: arr.pop()
+        
+dfs()
