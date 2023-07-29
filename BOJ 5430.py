c3ce6334ca862e548f4b15a212c7a4bda5c438bd
srcho01 <seorim5306@gmail.com>
2023-07-30 01:52:41 +0900

    230730

---
+++ BOJ 5430.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    f = input().strip()
+    n = int(input())
+    l = input().strip()[1:-1].split(",")
+    nums = [] if l[0] == '' else list(map(int, l))
+    
+    reverse = False
+    start = 0
+    end = n
+    
+    for cmd in f:
+        if cmd == 'R':
+            reverse = False if reverse else True
+        else:
+            if reverse:
+                end -= 1
+            else:
+                start += 1
+                
+            if start > end:
+                print('error')
+                break
+    else:
+        nums = nums[start:end]
+        if reverse: nums = nums[::-1]
+                
+        print("[", end='')
+        for i in nums[:-1]:
+            print(i, end='')
+            print(",", end='')
+        if len(nums) > 0: print(nums[-1], end='')
+        print("]")
