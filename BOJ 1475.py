4751853bf55396157d38d505964a3c2368512588
srcho01 <seorim5306@gmail.com>
2023-08-16 21:21:20 +0900

    1475 수정

--- BOJ 1475.py
+++ BOJ 1475.py
@@ -5,15 +5,8 @@ cnt = [0 for _ in range(9)]
 
 for c in s:
     n = int(c)
-    if n == 9:
-        n = 6
+    if n == 9: n = 6
     cnt[n] += 1
     
 cnt[6] = math.ceil(cnt[6] / 2)
-ans = []
-
-for i in cnt:
-    if i > 0:
-        ans.append(i)
-        
-print(max(ans))
+print(max(cnt))
