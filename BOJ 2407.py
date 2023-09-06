997099a28146fdb50f709f0d69bfdb58d83416e2
srcho01 <seorim5306@gmail.com>
2023-09-07 00:01:42 +0900

    230907

---
+++ BOJ 2407.py
@@ -0,0 +1,14 @@
+def C(n, r):
+    r = r if 2 * r < n else n - r
+    ret = 1
+    
+    for i in range(n, n-r, -1):
+        ret *= i
+    
+    for i in range(1, r+1):
+        ret //= i
+        
+    return ret
+
+n, m = map(int, input().split())
+print(C(n,m))
