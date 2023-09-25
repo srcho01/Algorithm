e4a359f9b6ad9b3c8b55cca6a69222accdc58875
srcho01 <seorim5306@gmail.com>
2023-09-25 20:52:26 +0900

    230925 수정

--- BOJ 10972.py
+++ BOJ 10972.py
@@ -10,15 +10,14 @@ while i >= 0 and p[i] >= p[i+1]:
 
 if i == -1:
     print(-1)
-    exit()
-elif i == n-1:
-    j = n-1
 else:
-    j = i
-    for k in range(i+1, n):
-        if p[i] < p[k]:
-            j += 1 
+    j = n-1
+    for x in p[n-1:i:-1]:
+        if p[i] > x:
+            j -= 1
+        else:
+            break
 
-p[i], p[j] = p[j], p[i]
-p = p[:i+1] + p[i+1:][::-1]
-print(*p)
+    p[i], p[j] = p[j], p[i]
+    p = p[:i+1] + p[i+1:][::-1]
+    print(*p)
