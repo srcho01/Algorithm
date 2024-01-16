59cabcb6c2ede874ba1f31fe7d2430995cdd5d70
srcho01 <seorim5306@gmail.com>
2024-01-16 21:34:32 +0900

    240116

--- BOJ 1916.py
+++ BOJ 1916.py
@@ -1,19 +1,32 @@
 import sys
+import heapq as hq
+from math import inf
 
 input = sys.stdin.readline
-n, l = map(int, input().split())
-water = sorted(list(map(int, input().split())))
 
-cnt = 0
-tape = 0
+n = int(input())
+m = int(input())
+city = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v, w = map(int, input().split())
+    city[u].append((v, w))
 
-for w in water:
-    if tape <= w - 0.5:
-        tape = w - 0.5
-        cnt += 1
-        tape += l
-    elif tape < w + 0.5:
-        cnt += 1
-        tape += l
+start, arrive = map(int, input().split())
 
-print(cnt)
+d = [inf] * (n+1)
+d[start] = 0
+heap = []
+hq.heappush(heap, (0, start))
+    
+while heap:
+    curr_cost, curr = hq.heappop(heap)
+    
+    if d[curr] < curr_cost: continue
+    
+    for next, next_cost in city[curr]:
+        new_cost = curr_cost + next_cost
+        if new_cost < d[next]:
+            d[next] = new_cost
+            hq.heappush(heap, (new_cost, next))
+            
+print(d[arrive])
