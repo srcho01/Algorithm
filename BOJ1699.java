945d7c02110825559231ea4cf0b6e6c446567434
srcho01 <seorim5306@gmail.com>
2024-08-31 19:09:54 +0900

    240831

---
+++ BOJ1699.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ1699 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 100_000 + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
