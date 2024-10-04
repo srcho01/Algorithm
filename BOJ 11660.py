60ea5ab7f57a2f14fc96b9998735c97c264f6698
srcho01 <seorim5306@gmail.com>
2024-10-05 02:05:56 +0900

    Change file name

--- BOJ 11660.py
+++ BOJ 11660.py
@@ -3,14 +3,18 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-chart = [[0] + list(map(int, input().split())) for _ in range(n)]
-chart = [[0] * (n+1)] + chart
+ps = [[0] * (n+1)]
+for _ in range(n):
+    ps.append([0] + list(map(int, input().split())))
 
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        chart[i][j] += chart[i-1][j] + chart[i][j-1] - chart[i-1][j-1]
-        
+    ps[1][i] += ps[1][i-1]
+    ps[i][1] += ps[i-1][1]
+    
+for i in range(2, n+1):
+    for j in range(2, n+1):
+        ps[i][j] += ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1]
+
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    
-    print(chart[x2][y2] - chart[x2][y1-1] - chart[x1-1][y2] + chart[x1-1][y1-1])
+    print(ps[x2][y2] - ps[x2][y1-1] - ps[x1-1][y2] + ps[x1-1][y1-1])
